202c86f0a699bd9c6edf3ab3bf69c0312c9bd398
testleaf1234 <gopi10042000@gmail.com>
2024-08-23 18:20:18 +0530

    Add files via upload

---
+++ weekEnd/Mobile.java
@@ -0,0 +1,30 @@
+package weekEnd;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+
+		String mobileName = "OPPO";
+		String mobileModal = "oppo f25 pro";
+		int mobileRate = 25000;
+		char mobileLogo = 'R';
+		float discountRate = 6.00005f;
+		double imbNo = 123567878733798l;
+		boolean isDisplay = true;
+		long warrantlyDays = 123456678123344l;
+
+		System.out.println(mobileName);
+		System.out.println(mobileModal);
+		System.out.println(mobileRate);
+		System.out.println(mobileLogo);
+		System.out.println(discountRate);
+		System.out.println(imbNo);
+		System.out.println(isDisplay);
+		System.out.println(warrantlyDays);
+
+		System.out.println(mobileName + "\n" + mobileModal + "\n" + mobileRate + "\n" + mobileLogo + "\n" + discountRate
+				+ "\n" + imbNo + "\n" + isDisplay + "\n" + warrantlyDays);
+
+	}
+
+}
